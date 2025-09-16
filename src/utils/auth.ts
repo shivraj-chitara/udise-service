be3f874612c35590888f4c47b33218b6ccd18803
Shivraj Chitara <shivraj.chitara@gmail.com>
2025-09-16 13:35:22 +0530

    cookie option secure false

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -6,7 +6,7 @@ export class AuthUtils {
     // set httpOnly cookie
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
+      secure: false,
       sameSite: 'lax',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
