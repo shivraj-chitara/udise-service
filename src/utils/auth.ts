ff17cbc5d6a83753994f9e84597f6a21d9c5acbe
Shivraj Chitara <shivraj.chitara@gmail.com>
2025-09-16 13:45:19 +0530

    setting sameSite none in cookie option

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -7,7 +7,7 @@ export class AuthUtils {
     res.cookie('token', token, {
       httpOnly: true,
       secure: false,
-      sameSite: 'lax',
+      sameSite: 'none',
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
   }
