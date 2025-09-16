9e8335e6f3c4aa7614b9a1c818a0cdbc48ae1f7f
Shivraj Chitara <shivraj.chitara@gmail.com>
2025-09-16 13:49:29 +0530

    cookie option updated

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -6,8 +6,8 @@ export class AuthUtils {
     // set httpOnly cookie
     res.cookie('token', token, {
       httpOnly: true,
-      secure: false,
-      sameSite: 'none',
+      secure: true, // required with SameSite=None (needs HTTPS)
+      sameSite: 'none', // allow cross-site cookies
       maxAge: 7 * 24 * 60 * 60 * 1000 // 7 days
     });
   }
